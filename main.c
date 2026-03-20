5d7ae75591db52468c6b14c35b6ddc44f4300134
BY50 <yudimoritaka@gmail.com>
2026-03-20 10:02:41 -0300

    Adicionado espaços e idade

--- main.c
+++ main.c
@@ -6,6 +6,9 @@ int main(){
     printf("17 anos");
     printf("\n");
     printf("Bruno Yudi");
+    printf("\n");
+    printf("19 anos");
+    printf("\n");
     printf("Rubens Henrique");
     return 0;
 };
